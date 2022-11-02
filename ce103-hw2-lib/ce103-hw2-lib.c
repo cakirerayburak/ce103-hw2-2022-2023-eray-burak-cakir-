7ada9ba2b751a67cdd84bc7483da6e5104ca4dcc
cakirerayburak <cakirerayburak@gmail.com>
2022-11-02 18:23:47 +0300

    Function created

--- ce103-hw2-lib/ce103-hw2-lib.c
+++ ce103-hw2-lib/ce103-hw2-lib.c
@@ -189,6 +189,18 @@ char* ce103_strrev(char* fiStr)
 int ce103_strlen(const char* fiStr)
 {
 	//TODO:Start from Here...
+	{
+	//We define an unsigned integer variable a and set it equal to zero
+		unsigned int a = 0;
+		while (*fiStr != '\0')
+		//When fiStr variable is not equal to '\0', a loop is created to increment variable a and fiStr pointer by one
+
+		{
+			a++;
+			fiStr++;
+		}
+		return a;
+	}
 	return -1;
 }
 
